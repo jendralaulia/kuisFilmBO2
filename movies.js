81fb98ff96278746af72fb65dcce0bbd6a599a1a
jendralaulia <jendral.aulia@gmail.com>
2026-03-25 18:03:49 +0700

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,35 @@
+const movieBank = [
+    {
+        // Adegan AADC (Pecahkan saja gelasnya)
+        videoId: "E3Tj5M04nKw", // Didapat dari link youtube.com/watch?v=E3Tj5M04nKw
+        startSeconds: 61,       // Mulai di detik 1:01 (61 detik)
+        endSeconds: 67,         // Berhenti di detik 1:07 (67 detik)
+        
+        // Babak 1: Judul
+        title: "Ada Apa Dengan Cinta?",
+        titleOptions: ["Ada Apa Dengan Cinta?", "Petualangan Sherina", "Eiffel I'm in Love", "Gie"],
+        
+        // Babak 2: Aktor (Bonus)
+        actor: "Dian Sastrowardoyo",
+        actorOptions: ["Dian Sastrowardoyo", "Nirina Zubir", "Luna Maya", "Shandy Aulia"],
+        
+        // Babak 3: Tahun (Jackpot)
+        year: "2002",
+        yearOptions: ["2000", "2002", "2004", "2006"]
+    },
+    {
+        // Adegan Warkop DKI (Jangkrik Boss) - CHIPS
+        videoId: "WlVvLpIokM0", 
+        startSeconds: 52,       
+        endSeconds: 56,         
+        
+        title: "CHIPS (Warkop DKI)",
+        titleOptions: ["CHIPS (Warkop DKI)", "Dongkrak Antik", "Gengsi Dong", "Maju Kena Mundur Kena"],
+        
+        actor: "Kasino",
+        actorOptions: ["Kasino", "Dono", "Indro", "Pancaran Sinar Petromaks"],
+        
+        year: "1982",
+        yearOptions: ["1979", "1982", "1985", "1988"]
+    }
+];
